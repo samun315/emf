f4a602c67d8c30c9819b6c5a8a1b49cd786e95b7
Samun Chowdhury <samun315@gmail.com>
2025-02-05 13:21:23 +0600

    login page modified

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -5,7 +5,7 @@
 <head>
     <title>Admin - Login </title>
     <meta charset="utf-8" />
-    <link rel="shortcut icon" href="{{ asset('assets/backend/media/logos/m22_logo.png') }}" />
+    <link rel="shortcut icon" href="{{ asset('assets/backend/media/logos/EMF-Final-Logo-footer.png') }}" />
     <!--begin::Fonts-->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
     <!--end::Fonts-->
@@ -33,24 +33,19 @@
                     <div class="d-flex flex-row-fluid flex-column text-center p-10 pt-lg-20">
                         <!--begin::Logo-->
                         <a href="/admin-login" class="py-9 mb-5">
-                            <img alt="Logo" src="{{ asset('assets/backend/media/logos/m22_logo.png') }}" class="h-150px" />
+                            <img alt="Logo" src="{{ asset('assets/backend/media/logos/EMF-Final-Logo-footer.png') }}" class="h-150px" />
 
                         </a>
                         <!--end::Logo-->
                         <!--begin::Title-->
-                        <h1 class="fw-bolder fs-2qx pb-5 pb-md-10" style="color: #986923;">Welcome to M22 Doctors
-                            Foundation</h1>
+                        <h1 class="fw-bolder fs-2qx pb-5 pb-md-10" style="color: #986923;">Welcome to Engineer Moqbul Foundation (EMF)</h1>
                         <!--end::Title-->
-                        <!--begin::Description-->
-                        <p class="fw-bold fs-2" style="color: #986923;">Discover Amazing M22 Doctors Foundation
-                            <br />with great build tools
-                        </p>
                         <!--end::Description-->
                     </div>
                     <!--end::Content-->
                     <!--begin::Illustration-->
-                    <div class="d-flex flex-row-auto bgi-no-repeat bgi-position-x-center bgi-size-contain bgi-position-y-bottom min-h-100px min-h-lg-300px"
-                        style="background-image: url(assets/backend/media/illustrations/sketchy-1/13.png"></div>
+                    {{-- <div class="d-flex flex-row-auto bgi-no-repeat bgi-position-x-center bgi-size-contain bgi-position-y-bottom min-h-100px min-h-lg-300px"
+                        style="background-image: url(assets/backend/media/illustrations/sketchy-1/13.png"></div> --}}
                     <!--end::Illustration-->
                 </div>
                 <!--end::Wrapper-->
@@ -72,7 +67,7 @@
                             <!--begin::Heading-->
                             <div class="text-center mb-10">
                                 <!--begin::Title-->
-                                <h1 class="text-dark mb-3">Sign In to M22 Doctors Foundation</h1>
+                                <h1 class="text-dark mb-3">Sign In to Engineer Moqbul Foundation (EMF)</h1>
                                 <!--end::Title-->
                             </div>
                             <!--begin::Heading-->
@@ -126,7 +121,7 @@
                 </div>
                 <!--end::Content-->
                 <!--begin::Footer-->
-                <div class="d-flex flex-center flex-wrap fs-6 p-5 pb-0">
+                {{-- <div class="d-flex flex-center flex-wrap fs-6 p-5 pb-0">
                     <!--begin::Links-->
                     <div class="d-flex flex-center fw-bold fs-6">
                         <a href="#" class="text-muted text-hover-primary px-2" target="_blank">About</a>
@@ -135,7 +130,7 @@
                     </div>
                     <!--end::Links-->
                 </div>
-                <!--end::Footer-->
+                <!--end::Footer--> --}}
             </div>
             <!--end::Body-->
         </div>
